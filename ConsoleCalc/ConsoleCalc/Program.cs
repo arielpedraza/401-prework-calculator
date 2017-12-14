4d31a651f2434bfc6305a08130b98d13a8130d49
Ariel R. Pedraza <pedrazaa2@gmail.com>
2017-12-13 21:36:44 -0800

    Refactoring a bit, added functionality to keep using calculator until finished with desired calculations. ie: can perform calculation on previous result.

--- ConsoleCalc/ConsoleCalc/Program.cs
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -6,25 +6,33 @@ namespace ConsoleCalc
     {
         static void Main(string[] args)
         {
+            string option = "";
+            double operand1, operand2, answer = 0.0;
+
             Console.WriteLine("Welcome to the world's most simple calculator!");
-            Console.WriteLine("Enter a number:");
-            string operand1 = Console.ReadLine();
-            string option = DisplayOptions();
-            Console.WriteLine("Enter a number:");
-            string operand2 = Console.ReadLine();
-            Console.WriteLine(Calculate(int.Parse(operand1), int.Parse(operand2), option));
+            Console.Write("Enter a number:");
+            operand1 = double.Parse(Console.ReadLine());
+            do
+            {
+                option = DisplayOptions();
+                Console.Write("Enter a number:");
+                operand2 = double.Parse(Console.ReadLine());
+                Console.WriteLine(Calculate(operand1, operand2, option, out answer));
+                operand1 = answer;
+            } while (option != "done");
             Console.ReadLine();
         }
 
         static string DisplayOptions()
         {
-            Console.WriteLine("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Done");
-            return Console.ReadLine();
+            Console.WriteLine("1. Add \n2. Subtract \n3. Multiply \n4. Divide \n5. Done");
+            string userInput = Console.ReadLine();
+            // if (userInput )
+            return userInput;
         }
 
-        static string Calculate(int operand1, int operand2, string option)
+        static string Calculate(double operand1, double operand2, string option, out double answer)
         {
-            double answer = 0.0;
             switch (option)
             {
                 case "1":
@@ -44,6 +52,7 @@ namespace ConsoleCalc
                     answer = operand1 / operand2;
                     break;
                 default:
+                    answer = 0.0;
                     break;
             }
             return String.Format("{0} {2} {1} = {3}", operand1, operand2, option, answer);
